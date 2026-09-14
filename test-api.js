fd5f86f4be6ea9d82e1224260ffc72f81ba36b52
Discovery Safaris <admin@discoverysafaris.com>
2026-09-14 21:06:55 +0530

    Remove test script

--- test-api.js
+++
@@ -1,5 +0,0 @@
-const fetch = require('node-fetch');
-fetch('http://localhost:3000/api/safaris')
-  .then(r => r.json())
-  .then(d => console.log('API Returned length:', d.length))
-  .catch(e => console.error(e.message));
